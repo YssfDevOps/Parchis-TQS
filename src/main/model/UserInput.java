b7a27e88fb68a8a9b9efb8ae29c6e030cc1f2336
YssfDevOps <youssef.cah.7912@gmail.com>
2024-11-30 18:20:25 +0100

    Maybe works

--- src/main/model/UserInput.java
+++ src/main/model/UserInput.java
@@ -1,4 +1,5 @@
 package main.model;
+
 import main.controller.GameController;
 import main.view.GameView;
 
@@ -6,31 +7,47 @@ import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.util.Scanner;
 
-// Control inputs of each player
 public class UserInput {
-  public static void setPlayerScanner(Player player, InputStream inputStream) throws Exception {
-    Field scannerField = Player.class.getDeclaredField("scanner");
-    scannerField.setAccessible(true);
-    Scanner scanner = new Scanner(inputStream);
-    scannerField.set(player, scanner);
+
+  public static void setPlayerScanner(Player player, InputStream inputStream) {
+    try {
+      Field scannerField = Player.class.getDeclaredField("scanner");
+      scannerField.setAccessible(true);
+      Scanner scanner = new Scanner(inputStream);
+      scannerField.set(player, scanner);
+    } catch (NoSuchFieldException | IllegalAccessException e) {
+      throw new RuntimeException("Failed to set player scanner", e);
+    }
   }
 
-  public static void setGameControllerView(GameController gameController, GameView view) throws Exception {
-    Field viewField = GameController.class.getDeclaredField("view");
-    viewField.setAccessible(true);
-    viewField.set(gameController, view);
+  public static void setGameControllerView(GameController gameController, GameView view) {
+    try {
+      Field viewField = GameController.class.getDeclaredField("view");
+      viewField.setAccessible(true);
+      viewField.set(gameController, view);
+    } catch (NoSuchFieldException | IllegalAccessException e) {
+      throw new RuntimeException("Failed to set game controller view", e);
+    }
   }
 
-  public static void setGameControllerDie(GameController gameController, Die die) throws Exception {
-    Field dieField = GameController.class.getDeclaredField("die");
-    dieField.setAccessible(true);
-    dieField.set(gameController, die);
+  public static void setGameControllerDie(GameController gameController, Die die) {
+    try {
+      Field dieField = GameController.class.getDeclaredField("die");
+      dieField.setAccessible(true);
+      dieField.set(gameController, die);
+    } catch (NoSuchFieldException | IllegalAccessException e) {
+      throw new RuntimeException("Failed to set game controller die", e);
+    }
   }
 
-  public static void setGameControllerScanner(GameController gameController, InputStream inputStream) throws Exception {
-    Field scannerField = GameController.class.getDeclaredField("scanner");
-    scannerField.setAccessible(true);
-    Scanner scanner = new Scanner(inputStream);
-    scannerField.set(gameController, scanner);
+  public static void setGameControllerScanner(GameController gameController, InputStream inputStream) {
+    try {
+      Field scannerField = GameController.class.getDeclaredField("scanner");
+      scannerField.setAccessible(true);
+      Scanner scanner = new Scanner(inputStream);
+      scannerField.set(gameController, scanner);
+    } catch (NoSuchFieldException | IllegalAccessException e) {
+      throw new RuntimeException("Failed to set game controller scanner", e);
+    }
   }
 }
